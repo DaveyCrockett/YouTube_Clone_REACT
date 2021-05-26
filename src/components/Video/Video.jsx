83cb63ade8add2e7b5a1b2d5996203640f0f4458
nmar30 <nmarcisso@live.com>
2021-05-26 11:43:12 -0400

    Added Video and SearchBar components

---
+++ src/components/Video/Video.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Video extends Component {
+    state = { 
+        video_id: '',
+        video_title: '',
+        video_description: '',
+     }
+    render() { 
+        return ( 
+            <div>
+                <iframe id="ytplayer" type="text/html" width="640" height="360"
+                src={`https://www.youtube.com/embed/${this.state.video_id}`}
+                frameborder="0"></iframe>
+                <h1>{this.state.video_title}</h1>
+                <p>{this.state.video_description}</p>
+            </div>
+         );
+    }
+}
+ 
+export default Video ;
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import SearchBar from './SearchBar/SearchBar'
+import Video from './Video/Video'
 
 class App extends Component {
-    state = { }
+    state = {
+
+     }
 
     render() {
         return (
+            <div>
+
+
             <h1>Our React App Using a Component</h1>
+            <SearchBar />
+            <Video />
+            </div>
         );
     }
 }
