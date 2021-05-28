28ace94ff5fb756f1100364a3c9f368aac289b39
nmar30 <nmarcisso@live.com>
2021-05-28 16:09:47 -0400

    Converted video component from class to function

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 
-class Video extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            video_title: '',
-            video_description: '',
-        } 
-     }
-    render() { 
+function Video(props){
         return ( 
             <div>
-                <iframe title={this.props.video_object.snippet.title} id="ytplayer" type="text/html" width="640" height="360"
-                src={`https://www.youtube.com/embed/${this.props.video_object.id.videoId}`}
+                <iframe title={props.video_object.snippet.title} id="ytplayer" type="text/html" width="640" height="360"
+                src={`https://www.youtube.com/embed/${props.video_object.id.videoId}`}
                 frameBorder="0"></iframe>
-                <h1>{this.props.video_object.snippet.title}</h1>
-                <p>{this.props.video_object.snippet.description}</p>
+                <h1>{props.video_object.snippet.title}</h1>
+                <p>{props.video_object.snippet.description}</p>
             </div>
          );
     }
-}
  
 export default Video ;
